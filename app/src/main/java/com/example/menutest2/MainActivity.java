b286abe0d1ac99fc289e1a6b78c6f167b79853bc
kimkingjae <dkskanrkd2@gmail.com>
2023-05-31 10:05:16 +0900

    민재짱

--- app/src/main/java/com/example/menutest2/MainActivity.java
+++ app/src/main/java/com/example/menutest2/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.menutest2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = findViewById(R.id.tv1);
+        registerForContextMenu(tv1);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contextmenu,menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.red:
+                tv1.setBackgroundColor(Color.RED);
+                return true;
+            case  R.id.green:
+                tv1.setBackgroundColor(Color.GREEN);
+                return true;
+            case  R.id.blue:
+                tv1.setBackgroundColor(Color.BLUE);
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.popupmenu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id){
+                    case R.id.smp:
+                        tv1.setText(R.string.text_smp);
+                        return true;
+                    case R.id.advp:
+                        tv1.setText(R.string.text_advp);
+                        return true;
+                    case R.id.db:
+                        tv1.setText(R.string.text_db);
+                        return true;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 }
